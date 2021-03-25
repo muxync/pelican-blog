f7910da261cd101537fa82a1baf6cf1473e59322
Mark Mulligan <muxync@muxync.net>
2021-03-25 13:38:46 -0400

    add pelican generated files, custom extras, and about page

---
+++ content/pages/about.md
@@ -0,0 +1,14 @@
+Title: About
+
+Mark Mulligan is a systems engineer with over 10 years experience using GNU/Linux and a mixed background in hardware/software.
+
+This static site (built with [Pelican](https://getpelican.com)) will act as a log of Mark's side projects.  Blog entries will cover a variety of topics such as:
+
+  * Systems Adminstration (Linux)
+  * Development (Python/Shell)
+  * Version Control (Git)
+  * Containerization (Docker Compose)
+  * Automation (GitLab CI/CD)
+  * Embedded Hardware (CircuitPython/Raspberry Pi)
+
+If this content proves useful for you please consider making a donation to the charity of your choice.  This site will remain free under the [MIT License](https://en.wikipedia.org/wiki/MIT_License).  Happy hacking!
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Mark Mulligan'
+SITENAME = 'MarkLog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Contact', 'mailto:contact@muxync.net'),)
+
+# Social widget
+SOCIAL = (('GitLab', 'https://gitlab.com/muxync'),
+          ('LinkedIn', 'https://www.linkedin.com/in/mark-mulligan-898b3962'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Theme
+THEME = os.path.join((os.path.dirname(os.path.realpath(__file__))), 'theme')
+
+# Extra
+STATIC_PATHS = ['extra/robots.txt', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {'extra/robots.txt': {'path': 'robots.txt'},
+                       'extra/favicon.ico': {'path': 'favicon.ico'}}
